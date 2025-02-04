3b55c6dfcc2a2a92cc80273e4046227519f2d5ff
maneetHTML <131334486+maneetHTML@users.noreply.github.com>
2025-02-04 18:20:12 +1300

    Add files via upload

---
+++ after.py
@@ -0,0 +1,23 @@
+
+name=input("Enter your name:")
+
+print("Hello",name)
+
+print("How are you feeling today:?")
+
+feeling=input()
+
+if feeling=="good":
+
+ print("Happy to hear")
+
+elif feeling=="sad":
+  
+  print("Don't worry")
+
+else:
+
+  print("I see. Sometimes it's hard to put feelings into words.")
+  sport = input('what sport do you like')
+print (sport, 'is a go sport')
+print("ok good by",name)
